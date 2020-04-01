d9a40642941b4721db41c9099bb0ed5a16c41cac
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-01 00:07:46 -0600

    skeleton

---
+++ scf-product-microservice/src/main/java/mx/unnamed/scf/ScfProductMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package mx.unnamed.scf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfProductMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScfProductMicroserviceApplication.class, args);
+	}
+
+}
