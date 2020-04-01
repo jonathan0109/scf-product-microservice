a1591732e1fb3e7ae3c1c5c7d1cb5dd69ec7f2b9
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-01 01:18:11 -0600

    model - product entity defined

--- scf-product-microservice/src/main/java/mx/unnamed/scf/model/Product.java
+++ scf-product-microservice/src/main/java/mx/unnamed/scf/model/Product.java
@@ -2,8 +2,16 @@ package mx.unnamed.scf.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
