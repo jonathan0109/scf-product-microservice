44fef5ce5cbc09c8b1d963b46f7b6645bce2b09a
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-04 01:07:01 -0600

    enable and configure the client

--- scf-product-microservice/src/main/java/mx/unnamed/scf/ScfProductMicroserviceApplication.java
+++ scf-product-microservice/src/main/java/mx/unnamed/scf/ScfProductMicroserviceApplication.java
@@ -2,8 +2,10 @@ package mx.unnamed.scf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ScfProductMicroserviceApplication {
 
 	public static void main(String[] args) {
