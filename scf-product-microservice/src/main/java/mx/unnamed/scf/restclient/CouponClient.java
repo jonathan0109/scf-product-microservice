66cf4acb017b771782d52895465bfd25de487d1f
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-18 04:00:25 -0500

    coupon client refactor

--- scf-product-microservice/src/main/java/mx/unnamed/scf/restclient/CouponClient.java
+++ scf-product-microservice/src/main/java/mx/unnamed/scf/restclient/CouponClient.java
@@ -1,16 +1,14 @@
 package mx.unnamed.scf.restclient;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import mx.unnamed.scf.model.Coupon;
 
-@FeignClient("COUPON-SERVICE")
-@RibbonClient("COUPON-SERVICE")
+@FeignClient("zuul-api-gateway")
 public interface CouponClient {
 	
-	@GetMapping("/couponapi/coupons/{code}")
+	@GetMapping("/coupon-service/couponapi/coupons/{code}")
 	Coupon getCoupon(@PathVariable("code") String code);
 }
