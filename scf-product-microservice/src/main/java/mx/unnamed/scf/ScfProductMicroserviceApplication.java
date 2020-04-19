db7a8af278f45304c952f5d5e7e53e7653943d91
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-19 15:27:35 -0500

    enable hystrix

--- scf-product-microservice/src/main/java/mx/unnamed/scf/ScfProductMicroserviceApplication.java
+++ scf-product-microservice/src/main/java/mx/unnamed/scf/ScfProductMicroserviceApplication.java
@@ -3,15 +3,16 @@ package mx.unnamed.scf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
 public class ScfProductMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScfProductMicroserviceApplication.class, args);
 	}
-
 }
